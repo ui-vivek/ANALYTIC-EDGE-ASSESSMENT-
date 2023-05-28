724400b508af4a9fc14de42062c09609598be7bf
Vivek singh <cu.vivek.1557@gmail.com>
2023-05-28 12:09:58 +0530

    formate and comments are added

--- src/components/CountryTable.jsx
+++ src/components/CountryTable.jsx
@@ -1,72 +1,67 @@
-import React, { useEffect, useState } from "react";
-import DataTable from "react-data-table-component";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
+import axios from 'axios';
 
 const CountryTable = () => {
-  const [countries, setCounries] = useState([]);
-  const [search, setSearch] = useState("");
-  const [filterCountries, setFilterCountries] = useState([]);
+  // State variables
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
+  // Fetch countries data from API
   const getCountries = async () => {
     try {
-      const responce = await axios.get("https://restcountries.com/v2/all");
-      setCounries(responce.data);
-      setFilterCountries(responce.data);
+      const response = await axios.get('https://restcountries.com/v2/all');
+      setCountries(response.data);
+      setFilteredCountries(response.data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  // Define table columns
   const columns = [
     {
-      name: "Country Name",
+      name: 'Country Name',
       selector: (row) => row.name,
       sortable: true,
     },
     {
-      name: "Country Native Name",
+      name: 'Country Native Name',
       selector: (row) => row.nativeName,
       sortable: true,
     },
     {
-      name: "Country Capital",
+      name: 'Country Capital',
       selector: (row) => row.capital,
       sortable: true,
     },
     {
-      name: "Country Flag",
+      name: 'Country Flag',
       selector: (row) => (
-        <img width={50} height={50} src={row.flag} alt="contary flag" />
+        <img width={50} height={50} src={row.flag} alt="country flag" />
       ),
     },
   ];
 
+  // Fetch countries data on component mount
   useEffect(() => {
     getCountries();
   }, []);
 
-  // useEffect(()=>{
-  //   const result=countries.filter(country=> {
-  //       return country.name.toLowerCase().match(search.toLocaleLowerCase())
-  //   })
-  //   setFilterCountries(result)
-  // },[])
-
+  // Filter countries based on search input
   useEffect(() => {
-    const result = countries.filter((country) => {
-      return country.name.toLowerCase().match(search.toLowerCase());
+    const filtered = countries.filter((country) => {
+      return country.name.toLowerCase().includes(search.toLowerCase());
     });
-    setFilterCountries(result);
+    setFilteredCountries(filtered);
   }, [search, countries]);
-  
 
   return (
     <DataTable
-      title={
-        <h1 className="d-flex flex-column align-items-center">Country List</h1>
-      }
+      title={<h1 className="d-flex flex-column align-items-center">Country List</h1>}
       columns={columns}
-      data={filterCountries}
+      data={filteredCountries}
       pagination
       fixedHeader
       fixedHeaderScrollHeight="28rem"
